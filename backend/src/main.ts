a366ec5a86d3b66d2c2e3d0b59dee7dbe01f72d8
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-07-09 22:59:34 +0530

    allowing all origin for sharing resources

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://mini-food-ordering-app-react-nest-js.vercel.app/',
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
   await app.listen(3000);
